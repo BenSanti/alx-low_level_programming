042f8440bc896b883b9e7212e0e62f3620ba8dc3
BenSanti <bensonchineduotti@gmail.com>
2023-02-16 09:18:12 -0800

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - a program or fo function that prints a line to the standard error
+ *
+ * Return: 1 when successful
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
+	return (1);
+}
