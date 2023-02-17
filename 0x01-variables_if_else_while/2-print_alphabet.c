e3ef210b1fe5e376eb4ef6f5a1735b704ff3abca
BenSanti <bensonchineduotti@gmail.com>
2023-02-17 05:40:21 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - it will print the alphabetic
+ *
+ * Return: Always (sucess)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
