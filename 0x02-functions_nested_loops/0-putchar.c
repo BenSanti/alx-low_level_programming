e60a37f28154bb136fe53cc0ae0d3b59f0b62b1a
BenSanti <bensonchineduotti@gmail.com>
2023-02-21 04:46:25 -0800

    to printputchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - This is the entry point in c
+ *
+ * Return: Always 0 when successful
+ *
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
