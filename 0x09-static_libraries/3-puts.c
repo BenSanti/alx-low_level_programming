6ea890708a9f7b2af857d5eaecd391e4b0477545
BenSanti <bensonchineduotti@gmail.com>
2023-03-10 05:39:28 -0800

    Static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+/**
+ * _puts - inputs character
+ * @s: inputs string
+ *
+ * Return: void
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
