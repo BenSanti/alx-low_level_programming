782f3fc3f653cbe368dd9cc19c8c0da7fa92a689
BenSanti <bensonchineduotti@gmail.com>
2023-03-20 18:33:01 -0700

    Define a new type struct dog with the following elements: char amd float

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,11 +16,19 @@ struct dog
 	float age;
 	char *owner;
 };
-
 /**
  * dog_t - typedef for struct dog
+ * void init_dog(struct dog *d, char *name, float age, char *owner):
+ * void print_dog(struct dog *d):
+ * typedef struct dog dog_t:
+ * void free_dog(dog_t *d):
+ * char *_strcpy(char *dest, char *src):
+ * int _strlen(char *s):
+ * @name: First member
+ * @age: the float or number
+ * @owner: the second character to enter
+ * @d: The number
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
